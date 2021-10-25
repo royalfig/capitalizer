20f0f6ade11072aed6328d6e76fdfc6055685f6a
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2021-10-25 12:33:12 -0400

    delete

--- src/lib/lib/articles.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const articles = ["a", "an", "the"];
-exports.default = articles;
--- src/lib/lib/cmsSpecialWords.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const cmsSpecialWords = ["and", "as", "but", "for", "or", "nor"];
-exports.default = cmsSpecialWords;
